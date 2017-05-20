51a76be3ae34e0e418359a90bdc9d56e7d254968
karthikvut <karthikjobmail1@gmail.com>
2017-05-20 14:03:42 -0500

    Added arithmetic operations and scientific operations

---
+++ operations/arithmeticOps.py
@@ -0,0 +1,20 @@
+"""Basic Arithmetic operations"""
+
+num1 = int(input('Enter your first number: '))
+num2 = int(input('Enter your second number: '))
+
+# Addition
+print('{0} + {1} = '.format(num1, num2))
+print(num1 + num2)
+
+# Subtraction
+print('{0} - {1} = '.format(num1, num2))
+print(num1 - num2)
+
+# Multiplication
+print('{0} * {1} = '.format(num1, num2))
+print(num1 * num2)
+
+# Division
+print('{0} / {1} = '.format(num1, num2))
+print(num1 / num2)
